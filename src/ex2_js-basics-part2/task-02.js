72dfccbf7ccc10a92244e5cc40964c23e7f9c821
Alexander <52191329+Alexandr100RB@users.noreply.github.com>
2020-03-24 21:58:26 +0300

    Deleted unwanted files

--- src/ex2_js-basics-part2/task-02.js
+++
@@ -1,9 +0,0 @@
-function showEveryElement(arr) {
-    for (let element of arr) {
-        console.log(arr[element]);
-    }
-    console.log(arr.length);
-    return;
-}
-
-module.exports = showEveryElement();
